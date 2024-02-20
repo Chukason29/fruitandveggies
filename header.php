244399dbc5f836d8d73e5a17e5b3ea573b173c34
Chukwuebuka Alaegbu <chukwuebukaalaegbu@gmail.com>
2024-02-20 20:46:19 +0100

    initial commit

---
+++ header.php
@@ -0,0 +1,19 @@
+<nav id="navbar">
+    <div>
+        <div id="logo">
+            <img src="images/fruits-and-veggies-logo.png" alt="fruits&veggies vegetables fruits" width="150px" height="80px">
+        </div>
+        <div id="menu-bar">
+            <div>
+                <a href="index">HOME</a>
+                <a href="about-us">ABOUT US</a>
+                <a href="blog">BLOG</a>
+                <a href="product-catalog">CATALOG</a>
+                <a href="">CONTACTS</a> 
+            </div>
+            <div>
+                <i class="fa-solid fa-magnifying-glass"></i>
+            </div>
+        </div>
+    </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="fruits Nigeria ecommerce shop online store ecommerce vegetables veggies store ">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Antic&family=Cherry+Bomb+One&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/6c1ada6317.js" crossorigin="anonymous"></script>
+    <title>FRUITS & VEGGIES - HOME</title>
+</head>
+<body>
+    <?php include "header.php"?>
+    <section id="main-banner">
+        <div>
+            <div id="main-banner-left">
+                <p id="first-writ"><strong>PROVIDING QUALITY PRODUCTS</strong></p>
+                <h2 id="header-writ">ORGANIC FRUITS</h2>
+                <h2>AND VEGETABLES</h2>
+                <p id="healthy"><strong>100% HEALTHY & AFFORDABLE</strong></p>
+                <a href="http://">SHOP NOW</a>
+            </div>
+            <div id="main-banner-right">
+                <img src="images/main-banner-fruit.png" alt="fruits basket vegetables veggies" width="750" height="600">
+            </div>
+        </div>
+    </section>
+</body>
+</html>
