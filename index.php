aadd098b212acf85625a48079094fcf349c38352
Chukwuebuka Alaegbu <chukwuebukaalaegbu@gmail.com>
2024-02-22 15:16:27 +0100

    added new links to index

--- index.php
+++ index.php
@@ -7,10 +7,10 @@
     <meta name="keywords" content="fruits Nigeria ecommerce shop online store ecommerce vegetables veggies store ">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="./css/style.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <!--<link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Antic&family=Cherry+Bomb+One&display=swap" rel="stylesheet">
-    <script src="https://kit.fontawesome.com/6c1ada6317.js" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/6c1ada6317.js" crossorigin="anonymous"></script>-->
     <title>FRUITS & VEGGIES - HOME</title>
 </head>
 <body>
@@ -22,12 +22,55 @@
                 <h2 id="header-writ">ORGANIC FRUITS</h2>
                 <h2>AND VEGETABLES</h2>
                 <p id="healthy"><strong>100% HEALTHY & AFFORDABLE</strong></p>
-                <a href="http://">SHOP NOW</a>
+                <a href="http://">SHOP NOW</a>                
             </div>
             <div id="main-banner-right">
-                <img src="images/main-banner-fruit.png" alt="fruits basket vegetables veggies" width="750" height="600">
+                <img src="images/main-fruit-banner.png" alt="fruits basket vegetables veggies" width="750" height="750">
             </div>
         </div>
+    </section>
+    <section id="fruit-veggies">
+        <div id="fruit-veggies-left">
+            <h2>WHY FRUITS AND VEGGIES?</h2>
+        </div>
+        <div id="fruit-veggies-right">
+            <div id="fruit-veggies-right-inner">
+                <div class="veggies-block">
+                    <img src="images/home-svg.jpg" alt="">
+                    <p class="veggies-block-p">MODERN FARM</p>
+                    <p class="veggies-block-p2">We get our farm products from companies with modern technologies.</p>
+                </div>
+                <div class="veggies-block">
+                    <img src="images/fresh-svg.jpg" alt="">
+                    <p class="veggies-block-p">FARM FRESH</p>
+                    <p class="veggies-block-p2">We deliver our products always fresh & organic so that you could enjoy your meal.</p>
+                </div>
+                <div class="veggies-block">
+                    <img src="images/organic-svg.jpg" alt="">
+                    <p class="veggies-block-p">100% ORGANIC</p>
+                    <p class="veggies-block-p2">We adhere to our organic philosophy, which implies avoiding pesticides.</p>
+                </div>
+                <div class="veggies-block">
+                    <img src="images/cost-svg.jpg" alt="">
+                    <p class="veggies-block-p">COST FRIENDLY</p>
+                    <p class="veggies-block-p2">Our give the best prices of farm fresh fruits & vegetables to met your taste expectation.</p>
+                </div>
+            </div>
+        </div>
+        
+    </section>
+    <section id="fruit-hands">
+        <div id="fruit-hands-left">
+            <p class="best-fruit">BEST FRUITS AND VEGGIES IN THE COUNTRY</p>
+            <h1 class="our-store">OUR STORE</h1>
+            <h2 class="who-we-are">WHO WE ARE</h2>
+            <div id="home-aboutus">
+                <p>ABOUT</p>
+                <p>OUR MISSION</p>
+                <p>OUR VISION</p>
+            </div>
+        </div>
+        <div id="fruit-hands-right"></div>
     </section>
 </body>
 </html>
