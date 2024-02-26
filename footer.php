a96aaafc4826056f06c8f2c5ac48c77a15adf24a
Chukwuebuka Alaegbu <chukwuebukaalaegbu@gmail.com>
2024-02-26 14:52:40 +0100

    added the footer

--- footer.php
+++ footer.php
@@ -1,3 +1,25 @@
-        <script src="./js/main.js"></script>
+       <section id="footer">
+            <div id="footer-div">
+                <div>
+                    <p id="footer-stay">STAY</p>
+                    <p id="footer-connected">CONNECTED</p>
+                    <p id="footer-subscribe">Subscribe to our newsletter</p>
+                </div>
+                <div>
+                    <div id="footer-socials">
+                        <a href="http://"><i class="fa-brands fa-tiktok"></i></a>
+                        <a href="http://"><i class="fa-brands fa-instagram"></i></a>
+                        <a href="http://"><i class="fa-brands fa-facebook-f"></i></a>
+                    </div>
+                    <form action="">
+                        <div>
+                            <input type="email" name="footer-mail" id="footer-mail" placeholder="Email your email address">
+                            <button type="submit">SUBSCRIBE</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+       </section>
+       <script src="./js/main.js"></script>
     </body>
 </html>
