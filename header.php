0c85fdc9a68142ab83aed884f1f7ff8c4585647a
Chukwuebuka Alaegbu <chukwuebukaalaegbu@gmail.com>
2024-02-26 13:36:54 +0100

    added the footer

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <nav id="navbar">
     <div>
         <div id="logo">
-            <img src="images/fruits-and-veggies-logo.png" alt="fruits&veggies vegetables fruits" width="150px" height="80px">
+            <img src="images/fruits-and-veggies-logo.png" alt="fruits&veggies vegetables fruits" width="175px" height="80px">
         </div>
         <div id="menu-bar">
             <div>
